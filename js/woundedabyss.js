6f72d8fae1ab8932c8745245047c650d3ff9d217
KeyboardFire <andy@keyboardfire.com>
2014-08-02 05:10:45 -0500

    get something onto the screen!

--- js/woundedabyss.js
+++ js/woundedabyss.js
@@ -1,8 +1,18 @@
 var WoundedAbyss = {
     init: function(cnv) {
+        var dom = {
+            cnv: cnv, ctx: cnv.getContext('2d'),
+            w: 800, h: 600
+        };
+
+        dom.cnv.width = dom.w;
+        dom.cnv.height = dom.h;
+
         return {
             play: function() {
-                // foo
+                var img = new Image();
+                img.onload = function() { dom.ctx.drawImage(img, 0, 0); };
+                img.src = 'img/tree.png';
             }
         };
     }
