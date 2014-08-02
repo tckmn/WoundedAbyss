2bd6af72ff57143306b10061cde8dd3f1cb38c53
KeyboardFire <andy@keyboardfire.com>
2014-08-02 04:29:39 -0500

    no phaser, using vanilla js

--- woundedabyss.js
+++ woundedabyss.js
@@ -1,19 +1,14 @@
-function preload() {
-    // foo
-}
-
-function create() {
-    // bar
-}
-
-function update() {
-    // baz
-}
-
-window.onload = function() {
-    var game = new Phaser.Game(800, 600, Phaser.AUTO, {
-        preload: preload,
-        create: create,
-        update: update
-    });
+var WoundedAbyss = {
+    init: function(cnv) {
+        return {
+            play: function() {
+                // foo
+            }
+        };
+    }
 };
+
+window.addEventListener('load', function() {
+    var cnv = document.getElementById('woundedabyss');
+    WoundedAbyss.init(cnv).play();
+});
